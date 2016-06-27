6edbf2e378282d3545988fd7140d40d31eb8d558
rohanrk07 <rohan.rk07@gmail.com>
2016-06-27 10:24:32 +0530

    Adding JS Files

---
+++ js/model.js
@@ -0,0 +1,59 @@
+if(!fbSearch.model){
+	fbSearch.model = (function(){
+
+		function Model(storage){
+			this.oStorage = {};
+			this.setLocalStorage = function(store){
+				localStorage.setItem("fbFavPages", JSON.stringify(store));
+			}
+			this.getLocalStorage = function(){
+				return JSON.parse(localStorage.getItem("fbFavPages"));
+			}
+		}
+
+		Model.prototype.createStorage = function(data, query){
+			var sQuery = query.toLowerCase();
+			if(!this.oStorage[sQuery]){
+				this.oStorage[sQuery] = data;
+			}
+		}
+
+		Model.prototype.findStorage = function(query){
+			var sQuery = query.toLowerCase();
+			if(this.oStorage[sQuery]){
+				return this.oStorage[sQuery];
+			}else{
+				return false;
+			}
+		}
+
+		Model.prototype.checkFavourite = function(id){
+			var store = JSON.parse(localStorage.getItem("fbFavPages"));
+			if(store && store.indexOf(id) !== -1){
+				return true;
+			}else{
+				return false;
+			}
+		}
+
+		Model.prototype.addFavItem = function(id, context){
+			var store = JSON.parse(localStorage.getItem("fbFavPages"));
+			if(!store){
+				localStorage.setItem("fbFavPages", "[]")
+			}
+			store.push(id);
+			this.setLocalStorage(store);
+			context.toggleFavState(id, "filled");
+		}
+
+		Model.prototype.removeFavItem = function(id, context){
+			var store = JSON.parse(localStorage.getItem("fbFavPages"));
+			var index = store.indexOf(id);
+			store.splice(index, 1);
+			this.setLocalStorage(store);
+			context.toggleFavState(id, "");
+		}
+
+		return Model;
+	})(window);
+}
---
+++ js/utils.js
@@ -0,0 +1,58 @@
+var fbSearch = {}; //Global object
+if(!fbSearch.utils){
+	fbSearch.utils = (function(){
+
+		var sAppKey = "1513453565582386",
+            sAppSecretKey = "a877266262b417696385aa55b92e959c",
+            sAccessToken = sAppKey + "|" + sAppSecretKey,
+            counter = 0,
+            fields = "global_brand_page_name,category,location,about,cover,link,website,likes";
+
+        function FBPageSearch(query, fnCallback, context, bPageId){
+        	var sCallback = "callback"+ counter++;
+            if(bPageId){
+                var sUrl = "https://graph.facebook.com/"+query+"?access_token="+sAccessToken+"&callback="+sCallback+"&fields="+fields;
+            }else{
+                var sUrl = "https://graph.facebook.com/search?q="+query+"&type=page&access_token="+sAccessToken+"&callback="+sCallback+"&fields="+fields;
+            }
+        	
+
+        	addScript(sUrl, sCallback, context, fnCallback);
+        }
+
+        function addScript(sUrl, sCallback, context, fnCallback){
+        	window[sCallback] = function(data){
+        		fnCallback.call(context, data);
+        		window.document.querySelector('#' + sCallback).remove();
+        	}
+        	var oHead = window.document.querySelector('head');
+            var oScript = window.document.createElement('script');
+            oScript.src = sUrl;
+            oScript.id = sCallback;
+            oHead.appendChild(oScript);
+        }
+
+        function sortData(field, data, sortOrder){
+            data.sort(function(a, b){
+                if(sortOrder === "desc"){
+                    if (a[field] < b[field])
+                        return 1;
+                    if (a[field] > b[field])
+                        return -1;
+                    return 0;
+                }else{
+                    if (a[field] < b[field])
+                        return -1;
+                    if (a[field] > b[field])
+                        return 1;
+                    return 0;
+                }
+            })
+        }
+		
+		return {
+			FBPageSearch : FBPageSearch,
+            sortData : sortData
+		};
+	})(window);
+}
